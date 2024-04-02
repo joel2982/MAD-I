858d5523948d03b4f79c46ece7928ddd8877b631
joel2982 <jjoel9958@gmail.com>
2024-04-02 15:46:40 +0530

    Week5 delete left

--- Week5/app.py
+++ Week5/app.py
@@ -34,9 +34,76 @@ class Enrollments(db.Model):
 @app.route('/')
 def home():
     students = Student.query.all()
-    print(students)
-    return render_template('student.html',student_data = students)
+    if students:
+        return render_template('studentList.html',student_data = students)
+    else:
+        return render_template('noStudent.html')
+    
+@app.route('/student/<int:student_id>')
+def view(student_id):
+    student = db.session.query(Student).filter_by(student_id =student_id).one()
+    return render_template('viewStudent.html',student = student)
+
+@app.route('/student/create/',methods = ['GET','POST'])
+def createStudent():
+    if request.method == 'GET':
+        return render_template('addStudent.html')
+    elif request.method == 'POST':
+        roll_number = request.form['roll']
+        first_name = request.form['f_name']
+        last_name = request.form['l_name']
+        courses = request.form.getlist('courses')
+
+        check_duplicate = db.session.query(Student).filter_by(roll_number = roll_number).first()
+        if check_duplicate:
+            return render_template('studentExisits.html')
+        
+        try:
+            new_student = Student(roll_number = roll_number, first_name = first_name, last_name = last_name)
+            for course_id in courses:
+                course = db.session.query(Course).filter_by(course_id = int(course_id[-1])).one()
+                new_student.courses.append(course)
+            db.session.add(new_student)
+            db.session.commit()
+        except Exception as E:
+            print(E)
+            print("ROLLING BACK")
+            db.session.rollback()
+        else:
+            print("COMMITTING CHANGES")            
+            db.session.commit()
+
+        return redirect('/')
+
+@app.route('/student/<int:student_id>/update',methods = ['GET','POST'])
+def update(student_id):
+    student_data = db.session.query(Student).filter_by(student_id = student_id).one()
+    if request.method == 'GET':
+        course_id = []
+        for course in student_data.courses:
+            course_id.append(course.course_id)
+        return render_template('updateStudent.html',student = student_data,course_id = course_id)
+    elif request.method == 'POST':
+        first_name = request.form['f_name']
+        last_name = request.form['l_name']
+        courses = request.form.getlist('courses')
+        
+        try:
+            student_data.first_name = first_name
+            student_data.last_name = last_name
+            student_data.courses = []
+            for course_id in courses:
+                course = db.session.query(Course).filter_by(course_id = int(course_id[-1])).one()
+                student_data.courses.append(course)
+            db.session.commit()
+        except Exception as E:
+            print(E)
+            print("ROLLING BACK")
+            db.session.rollback()
+        else:
+            print("COMMITTING CHANGES")            
+            db.session.commit()
+        return redirect('/')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug = True)
