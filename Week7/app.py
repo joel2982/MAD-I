33ba1819e3ec6067da5ef6f3a6d404a44dcc92e4
joel2982 <jjoel9958@gmail.com>
2024-07-04 04:41:37 +0530

    week7 complete

--- Week7/app.py
+++ Week7/app.py
@@ -83,7 +83,53 @@ def all_courses():
     courses = Course.query.all()
     return render_template('course_list.html',courses=courses)
 
+@app.route("/course/<int:course_id>")
+def display_course(course_id):
+    course = Course.query.get(course_id)
+    return render_template('display_course.html',course=course)
 
+
+@app.route("/course/create",methods=['GET','POST'])
+def create_course():
+    if request.method == 'POST':
+        course_code = request.form.get('course_code')
+        course_name = request.form.get('course_name')
+        course_description = request.form.get('course_description',None)
+        duplicate = Course.query.filter_by(course_code=course_code).first()
+        if duplicate:
+            return render_template('course_exists.html')
+        else:
+            new_course = Course(
+                    course_code=course_code,
+                    course_name=course_name,
+                    course_description=course_description
+                )
+            db.session.add(new_course)
+            db.session.commit()
+            return redirect('/courses')
+    else:
+        return render_template('create_course.html')
+    
+@app.route("/course/<int:course_id>/update",methods=['GET','POST'])
+def update_course(course_id):
+    if request.method == 'POST':
+        course = Course.query.get(course_id)
+        course_name = request.form.get('course_name')
+        course_description = request.form.get('course_description',None)
+        course.course_name = course_name
+        course.course_description = course_description
+        db.session.commit()
+        return redirect('/courses')
+    else:
+        course = Course.query.get(course_id)    
+        return render_template('update_course.html',course=course)
+
+@app.route("/course/<int:course_id>/delete")
+def delete_course(course_id):
+    course = Course.query.get(course_id)
+    db.session.delete(course)
+    db.session.commit()
+    return redirect('/courses')
 
 if __name__ == '__main__':
     app.run(debug=True)
